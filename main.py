ad211dc8b46bc394450fa1d2e98349c4cd85cb81
王瑞庆 <wangruiqing@4399inc.com>
2018-10-19 18:11:57 +0800

    login

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+from urllib.parse import urlparse
+import json
 
 baseUrl = {
     # {"mobile":"15659663812","captcha_value":"","captcha_hash":""}
@@ -25,6 +27,14 @@ res = requests.get('http://baidu.com', headers=headers, data=data)
 
 class login_info_obj(object):
     login_cookie = ""
+    eleme_url = ""
+    user_info = {}
+
+    def __init__(self, qq, start_url):
+        with open("./tmp/userInfo/qq_%s.json" % qq, 'r', encoding='utf-8') as content:
+            print(content)
+            self.user_info = json.load(content)
+        self.eleme_url = start_url
 
     def get_cookie(self):
         return self.login_cookie
@@ -69,6 +79,68 @@ class login_info_obj(object):
             sn = '29e47b57971c1c9d'
         return "eosid=" + str(int(sn, 16))
 
+    def moblie_send_code(self, mobile):
+        request_url = "https://h5.ele.me/restapi/eus/login/mobile_send_code"
+        request_data = {
+            'captcha_hash': '',
+            'captcha_value': '',
+            'mobile': mobile
+        }
+        request_header = {
+            'cookie': self.get_cookie()
+        }
+        # {"validate_token":"a4819b0386f4ac313dd71037bd8258e233a96b5f4ac002451f23bcc0b208c05f"}
+        res = requests.post(request_url, data=request_data, headers=request_header)
+        return json.loads(res)['validate_token']
+
+    def login_by_mobile(self, code):
+        request_url = "https://h5.ele.me/restapi/eus/login/login_by_mobile"
+        request_data = {
+            'mobile': "15659663812",
+            'validate_code': code,
+            'validate_token': "a4819b0386f4ac313dd71037bd8258e233a96b5f4ac002451f23bcc0b208c05f"
+        }
+        request_header = {
+            'cookie': self.get_cookie()
+        }
+        res = requests.post(request_url, data=request_data, headers=request_header)
+        self.set_cookie(requests.cookies.RequestsCookieJar())  # sid
+        return json.loads(res.text)
+
+    def open_page(self, sn, sign):
+        """
+
+        :param sn:
+        :param sign:
+        """
+        request_url = "https://h5.ele.me/restapi/marketing/promotion/weixin/" + self.user_info.openid
+        request_data = {
+            {
+                'device_id': "",
+                'group_sn': sn,
+                'hardware_id': "",
+                'method': "phone",
+                'phone': "",
+                'platform': 4,
+                'sign': self.user_info['eleme_key'],
+                'track_id': "",
+                'unionid': "fuck",
+                'weixin_avatar': self.user_info['figureurl_2'],
+                'weixin_username': self.user_info['nickname']
+            }
+        }
+        request_header = {
+            "x-shard": self.get_xshard(sn),
+            'cookie': "SID=" + self.cookie.sid
+        }
+        res = requests.post(request_url, data=request_data, headers=request_header)
 
 if __name__ == '__main__':
-    print(login_info_obj.get_xshard())
+    start_url = "https://h5.ele.me/hongbao/#hardware_id=&is_lucky_group=True&lucky_number=8&track_id=&platform=0&sn=10fcda587ea2d807&theme_id=1969&device_id=&refer_user_id=1097914722"
+    urldic = urlparse(start_url)
+    arr = urldic.fragment.split('&')
+    sn = filter(lambda x: 'sn' == x[:2], arr)
+    print(sn)
+    print(arr)
+    # a = login_info_obj(98709484, start_url)
+    # print(a.user_info)
---
+++ todo.md
@@ -0,0 +1,12 @@
+
+## 获取信息
+1. openid: "openid", // QQ或者WX授权登录饿了么之后，从cookie中可得openid
+2. sign: "sign", // QQ或者WX授权登录饿了么之后，从cookie中可得eleme_key就是sign
+3. sid: "" // 接码时传空
+
+4. 获取手机认证后的cookie
+
+## 领取红包
+
+## 记录信息
+1. 写入文本
