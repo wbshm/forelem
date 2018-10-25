e8fc8a52800a3ba103828e37e49665174309894b
hm <98709484@qq.com>
2018-10-25 22:35:29 +0800

    fix: finish work

--- main.py
+++ main.py
@@ -5,42 +5,23 @@ import requests
 from urllib.parse import urlparse
 import json
 
-baseUrl = {
-    # {"mobile":"15659663812","captcha_value":"","captcha_hash":""}
-    'send_code': 'https://h5.ele.me/restapi/eus/login/mobile_send_code',
-
-    # {"mobile":"15659663812","validate_code":"772605","validate_token":"003dd9da70846255f81e8f3605ec568e6326e40619ceaa34b3ce90a5e1d92e3e"}
-    'login_mobile':
-        "https://h5.ele.me/restapi/eus/login/login_by_mobile",
-
-    'userinfo':
-        'https://waltz.ele.me/qq/userinfo/?code=71804A0678A3780983791B0C5A324674',
-}
-
-# code: 71804A0678A3780983791B0C5A324674
-headers = {
-    "user-agent":
-        "Mozilla/5.0 (Linux; Android 7.0; MIX Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/57.0.2987.132 MQQBrowser/6.2 TBS/044004 Mobile Safari/537.36 V1_AND_SQ_7.5.0_794_YYB_D QQ/7.5.0.3430 NetType/WIFI WebP/0.3.0 Pixel/1080",
-
-}
-
-data = {}
-res = requests.get('http://baidu.com', headers=headers, data=data)
-
-
 
 class login_info_obj(object):
     login_cookie = ""
     eleme_url = ""
     user_info = {}  # 用户信息
-    luck_number = 0  # 幸运的礼包码
+    lucky_number = 0  # 幸运的礼包码
     sn = ""
+    qq = ""
+    SID = ""
 
     def __init__(self, qq, start_url):
+        self.qq = qq
         try:
-            with open("./tmp/userInfo/qq_%s.json" % qq, 'r', encoding='utf-8') as content:
-                print(content)
+            with open("./tmp/userInfo/qq_%s.json" % self.qq, 'r', encoding='utf-8') as content:
                 self.user_info = json.load(content)
+                if 'SID' in self.user_info:
+                    self.SID = self.user_info['SID']
         except FileNotFoundError:
             print('File Not Found Error')
             exit(sys._getframe().f_lineno)
@@ -50,50 +31,31 @@ class login_info_obj(object):
     def do_it(self):
         res = self.open_page()
         try:
+            print(self.lucky_number)
             result = res.json()
-            #{"message":"用户验证失败","name":"PHONE_IS_EMPTY"}
+            # {"message":"用户验证失败","name":"PHONE_IS_EMPTY"}
             if 'name' in result and result['name'] == 'PHONE_IS_EMPTY':
-                self.check_phone()
+                phone_num = self.check_phone()
+                if phone_num == False:
+                    print('phone num error')
+                    exit(sys._getframe().f_lineno)
+                validate_token = self.mobile_send_code(phone_num)
+                code = input("输入验证码:")
+                self.login_by_mobile(phone_num, code, validate_token)
+                res = self.open_page()
+                print(res)
+            elif 'promotion_records' in result:
+                count = len(result['promotion_records'])
+                if count == self.lucky_number - 1:
+                    print('the next one is biggest')
+                    exit(0)
+                elif count >= self.lucky_number - 1:
+                    print('the lucky page is gone')
+                    exit(-1)
         except ValueError:
             print('fatal error')
             exit(sys._getframe().f_lineno)
-
-    def get_cookie(self):
-        return self.login_cookie
-
-    def set_cookie(self, cookie):
-        self.login_cookie = cookie.strip()
-
-    def clean_cookie(self, cookie):
-        if (cookie[-1:] == '"' and cookie[0:1] == '"') or (cookie[-1:] == "'" and cookie[:1] == "'"):
-            cookie = cookie[1:-1]
-        return cookie
-
-    def get_red_packet(self):
-        pass
-
-    def get_request_body(self, sn, headimgurl=0, nickname=0):
-        body_info = {
-            'device_id': '',
-            'group_sn': sn,
-            'hardware_id': '',
-            'method': 'phone',
-            'phone': '',
-            'platform': 4,
-            'sign': 'this.cookie.sign',
-            'track_id': '',
-            'unionid': 'fuck',
-            'weixin_avatar': headimgurl,
-            'weixin_username': nickname,
-        }
-        return body_info
-
-    def get_header(self):
-        headers = {
-            'x-shard': self.get_xshard(),
-            'cookie': "SID=" + self.login_cookie.sid
-        }
-        return headers
+        return True
 
     @staticmethod
     def get_xshard(sn=''):
@@ -108,55 +70,55 @@ class login_info_obj(object):
             'captcha_value': '',
             'mobile': mobile
         }
-        request_header = {
-            'cookie': self.get_cookie()
-        }
         # {"validate_token":"a4819b0386f4ac313dd71037bd8258e233a96b5f4ac002451f23bcc0b208c05f"}
-        res = requests.post(request_url, data=request_data, headers=request_header)
-        return json.loads(res)['validate_token']
+        res = requests.post(request_url, data=request_data)
+        res_data = (json.loads(res.text))
+        if 'validate_token' in res_data:
+            return res_data['validate_token']
+        else:
+            print('需要图形验证码，换个手机号')
+            exit(sys._getframe().f_lineno)
 
-    def login_by_mobile(self, code):
+    def login_by_mobile(self, mobile, code, token):
         request_url = "https://h5.ele.me/restapi/eus/login/login_by_mobile"
         request_data = {
-            'mobile': "15659663812",
+            'mobile': mobile,
             'validate_code': code,
-            'validate_token': "a4819b0386f4ac313dd71037bd8258e233a96b5f4ac002451f23bcc0b208c05f"
+            'validate_token': token
         }
         request_header = {
-            'cookie': self.get_cookie()
         }
         res = requests.post(request_url, data=request_data, headers=request_header)
-        self.set_cookie(requests.cookies.RequestsCookieJar())  # sid
+        self.SID = res.cookies['SID']
+        self.user_info['SID'] = self.SID
         return json.loads(res.text)
 
     def open_page(self):
         request_url = "https://h5.ele.me/restapi/marketing/promotion/weixin/" + self.user_info['openid']
         request_data = {
-            {
-                'device_id': "",
-                'group_sn': self.sn,
-                'hardware_id': "",
-                'method': "phone",
-                'phone': "",
-                'platform': 4,
-                'sign': self.user_info['eleme_key'],
-                'track_id': "",
-                'unionid': "fuck",
-                'weixin_avatar': self.user_info['figureurl_2'],
-                'weixin_username': self.user_info['nickname']
-            }
+            'device_id': "",
+            'group_sn': self.sn,
+            'hardware_id': "",
+            'method': "phone",
+            'phone': "",
+            'platform': 4,
+            'sign': self.user_info['eleme_key'],
+            'track_id': "",
+            'unionid': "fuck",
+            'weixin_avatar': self.user_info['figureurl_2'],
+            'weixin_username': self.user_info['nickname']
         }
+
         request_header = {
             "x-shard": self.get_xshard(self.sn),
-            'cookie': "SID=" + self.cookie.sid
+            'cookie': "SID=%s" % str(self.SID)
         }
         res = requests.post(request_url, data=request_data, headers=request_header)
         return res
 
     def analysis_url(self):
         self.sn = self._get_url_params(self.eleme_url, 'sn=')
-        self.luck_number = self._get_url_params(self.eleme_url, 'luck_number=')
-        is_lucky_group = self._get_url_params(self.eleme_url, 'is_lucky_group=')
+        self.lucky_number = self._get_lucky_number()
         pass
 
     @staticmethod
@@ -166,34 +128,55 @@ class login_info_obj(object):
         try:
             return list(filter(lambda x: pick_key == x[:len(pick_key)], param_arr)).pop()[len(pick_key):]
         except IndexError:
-            print('param does not exist')
+            print('param %s does not exist' % pick_key)
             return ""
 
     def check_phone(self):
         phone_pat = re.compile('^(13\d|14[5|7]|15\d|166|17[3|6|7]|18\d)\d{8}$')
-        count = 3
+        count = 3  # 最大尝试次数
         while True:
             mobile = input("请输入电话号码,以获取验证短信:")
             res = re.search(phone_pat, mobile)
             if res:
                 print('正常手机号')
-                return True
+                return mobile
             else:
                 print('手机号错误')
                 count -= 1
             if count <= 0:
                 return False
 
+    def write_data(self):
+        with open("./tmp/userInfo/qq_%s.json" % self.qq, 'w') as f:
+            f.write(json.dumps(self.user_info))
+
+    def _get_lucky_number(self):
+        url = 'https://h5.ele.me/restapi/marketing/themes/3249/group_sns/%s' % self.sn
+        res = requests.get(url)
+        data = json.loads(res.text)
+        if 'lucky_number' in data:
+            return data['lucky_number']
+        else:
+            print('lucky number error')
+            exit(sys._getframe().f_lineno)
+
+
 if __name__ == '__main__':
-    start_url = "https://h5.ele.me/hongbao/#hardware_id=&is_lucky_group=True&lucky_number=8&track_id=&platform=0&sn=10fcda587ea2d807&theme_id=1969&device_id=&refer_user_id=1097914722"
-    qq = 987094841
-    try:
-        with open("./tmp/userInfo/qq_%s.json" % qq, 'r', encoding='utf-8') as content:
-            print(content)
-            user_info = json.load(content)
-            print(user_info)
-    except FileNotFoundError:
-        print('File Not Found Error')
-        # exit(sys._getframe().f_lineno)
-    print()
+    # res = requests.post('http://127.0.0.1')
+    # print(res.cookies['nihao1'])
+    # exit(1)
+    qq = 98709484
+    start_url = "https://h5.ele.me/hongbao/?from=groupmessage&isappinstalled=0#hardware_id=&is_lucky_group=True&lucky_number=0&track_id=&platform=0&sn=2a10ee28403b1c61&theme_id=3257&device_id=&refer_user_id=32936218"
+    # try:
+    #     with open("./tmp/userInfo/qq_%s.json" % qq, 'r', encoding='utf-8') as content:
+    #         print(content)
+    #         user_info = json.load(content)
+    #         print(user_info)
+    # except FileNotFoundError:
+    #     print('File Not Found Error')
+    #     # exit(sys._getframe().f_lineno)
+    # print()
+
+    test = login_info_obj(qq, start_url)
+    test.do_it()
     # a = login_info_obj(98709484, start_url)
